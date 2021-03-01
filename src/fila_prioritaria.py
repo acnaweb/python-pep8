2e50afad1f7b165e677e3e11c18fe773315e08d8
Antonio Carlos <ac@marketmining.com.br>
2021-02-28 22:44:24 -0300

    creating fila_base.py

--- src/fila_prioritaria.py
+++ src/fila_prioritaria.py
@@ -29,7 +29,8 @@ class FilaPrioritaria(FilaBase):
             estatistica["dia"] = dia
             estatistica["agencia"] = agencia
             estatistica["clientes_atendidos"] = self.clientes_atendidos
-            estatistica["quantidade_clientes_atendidos"] = \
+            estatistica["quantidade_clientes_atendidos"] = (
                 len(self.clientes_atendidos)
+            )
 
         return estatistica
